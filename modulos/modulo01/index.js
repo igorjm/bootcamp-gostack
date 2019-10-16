493cbcd406219e2d6a96f728533b4968904fd00b
Igor Melo <igorjmelo4@gmail.com>
2019-10-15 23:48:50 -0300

    node concepts

--- modulos/modulo01/index.js
+++ modulos/modulo01/index.js
@@ -2,22 +2,54 @@ const express = require('express');
 
 const server = express();
 
+server.use(express.json());
+
 // Query params = ?teste=1
 // Route params = /users/:1
 // Request body = { "name": "Igor", "email": "igor@gmail.com" }
 
-server.get('/users', (req, res) => {
+server.get('/users/name', (req, res) => {
   const name = req.query.name;
-
   return res.json({ message : `Hello ${name}` });
 });
 
 server.get('/users/:id', (req, res) => {
   const id = req.params.id;
-
   return res.json({ message : `ID: ${id}` });
 });
 
+const users = ['Igor', 'Cleber'];
 
+server.get('/users/:index', (req, res) => {
+  const { index } = req.params;
+  return res.json(users[index]);
+});
+
+// LIST ALL
+server.get('/users', (req, res) => {
+  return res.json(users);
+});
+
+// CREATE
+server.post('/users', (req, res) => {
+  const {name} = req.body;
+  users.push(name);
+  return res.json(users);
+});
+
+// EDIT
+server.put('/users/:index', (res, req) => {
+  const { index } = req.params;
+  const { name } = req.body;
+  users[index] = name;
+  return res.json(users);
+});
+
+// DELETE
+server.delete('/users/:index', (req, res) => {
+  const { index } = req.params;
+  users.splice(index, 1);
+  return res.send(200);
+})
 
 server.listen(3000);
