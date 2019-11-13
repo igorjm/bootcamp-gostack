4ea0c12591312efa5646a1e9652a220e5fadf6c6
Igor Melo <igorjmelo4@gmail.com>
2019-11-12 23:49:14 -0300

    starting new module | initial rn project

---
+++ modulos/react_native/modulo_06/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {
+  View,
+  Text,
+} from 'react-native';
+
+const styles = StyleSheet.create({
+
+});
+
+export default function App() {
+  return (
+    <View>
+      <Text>Welcome</Text>
+    </View>
+  );
+}
