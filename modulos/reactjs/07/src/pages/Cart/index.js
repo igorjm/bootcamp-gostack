0f215e214a23b60b0537f93d478119d8e64a9584
igor.melo <igorjmelo4@gmail.com>
2019-12-16 23:19:25 -0300

    styling home and cart

--- modulos/reactjs/07/src/pages/Cart/index.js
+++ modulos/reactjs/07/src/pages/Cart/index.js
@@ -1,9 +1,60 @@
 import React from 'react';
+import { MdRemoveCircleOutline, MdAddCircleOutline, MdDelete } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { Container, ProductTable, Total } from './styles';
 
 export default function Cart() {
   return (
-    <div />
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th></th>
+            <th>PRODUTO</th>
+            <th>QTD</th>
+            <th>SUBTOTAL</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img src="" title="" />
+            </td>
+            <td>
+              <strong>Titulo</strong>
+              <span>R$</span>
+            </td>
+            <td>
+              <div>
+                <button type="button">
+                  <MdRemoveCircleOutline size={20} color="#7159c1" />
+                </button>
+                <input type="number" value={1} readOnly />
+                <button type="button">
+                  <MdAddCircleOutline size={20} color="#7159c1" />
+                </button>
+              </div>
+            </td>
+            <td>
+              <strong>Total: R$</strong>
+            </td>
+            <td>
+              <button type="button">
+                <MdDelete size={20} color="#7159c1" />
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </ProductTable>
+
+      <footer>
+        <button type="button"> Finalizar pedido</button>
+        <Total>
+          <span>TOTAL</span>
+          <strong>R$</strong>
+        </Total>
+      </footer>
+    </Container>
   );
 }
--- modulos/reactjs/07/src/pages/Home/index.js
+++ modulos/reactjs/07/src/pages/Home/index.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { ProductList } from './styles';
 
 export default function Home() {
   return (
-    <div />
+    <ProductList>
+      <li>
+        <img src="" alt="Tenis" />
+        <strong>Tenis muito legal</strong>
+        <span>R$</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="FFF"/>
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
   );
 }
