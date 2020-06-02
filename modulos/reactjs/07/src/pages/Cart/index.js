383a3610fcb1f12591851cd4de4ad9d81155affb
igor.melo <igorjmelo4@gmail.com>
2020-06-01 23:15:13 -0300

    [+]: new testing module

--- modulos/reactjs/07/src/pages/Cart/index.js
+++ modulos/reactjs/07/src/pages/Cart/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { MdRemoveCircleOutline, MdAddCircleOutline, MdDelete } from 'react-icons/md';
+import {
+  MdRemoveCircleOutline,
+  MdAddCircleOutline,
+  MdDelete,
+} from 'react-icons/md';
 
 import { formatPrice } from '../../util/format';
 
@@ -9,15 +13,20 @@ import * as CartActions from '../../store/modules/cart/actions';
 import { Container, ProductTable, Total } from './styles';
 
 export default function Cart() {
-  const total = useSelector(state => formatPrice(state.cart.reduce((totalSum, product) => {
-      return totalSum + product.price * product.amount;
-    }, 0),
-  ));
+  const total = useSelector(state =>
+    formatPrice(
+      state.cart.reduce((totalSum, product) => {
+        return totalSum + product.price * product.amount;
+      }, 0)
+    )
+  );
 
-  const cart = useSelector(state => state.cart.map(product => ({
-    ...product,
-    subtotal: formatPrice(product.price * product.amount)
-  })));
+  const cart = useSelector(state =>
+    state.cart.map(product => ({
+      ...product,
+      subtotal: formatPrice(product.price * product.amount),
+    }))
+  );
 
   const dispatch = useDispatch();
 
@@ -45,7 +54,7 @@ export default function Cart() {
           {cart.map(product => (
             <tr>
               <td>
-                <img src={product.image} alt={product.title}/>
+                <img src={product.image} alt={product.title} />
               </td>
               <td>
                 <strong>{product.title}</strong>
@@ -66,7 +75,12 @@ export default function Cart() {
                 <strong>{product.subtotal}</strong>
               </td>
               <td>
-                <button type="button" onClick={() => dispatch(CartActions.removeFromCart(product.id))}>
+                <button
+                  type="button"
+                  onClick={() =>
+                    dispatch(CartActions.removeFromCart(product.id))
+                  }
+                >
                   <MdDelete size={20} color="#7159c1" />
                 </button>
               </td>
--- modulos/reactjs/07/src/pages/Home/index.js
+++ modulos/reactjs/07/src/pages/Home/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useDispatch, useSelector  } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { MdAddShoppingCart } from 'react-icons/md';
 import { formatPrice } from '../../util/format';
 import api from '../../services/api';
@@ -24,7 +24,7 @@ export default function Home() {
       const response = await api.get('products');
       const data = response.data.map(product => ({
         ...product,
-        priceFormatted: formatPrice(product.price)
+        priceFormatted: formatPrice(product.price),
       }));
       setProducts(data);
     }
@@ -34,20 +34,18 @@ export default function Home() {
 
   function handleAddProduct(id) {
     dispatch(CartActions.addToCartRequest(id));
-  };
+  }
 
   return (
     <ProductList>
-      { products.map( product => (
+      {products.map(product => (
         <li key={product.id}>
-          <img
-            src={product.image}
-            alt={product.title} />
+          <img src={product.image} alt={product.title} />
           <strong>{product.title}</strong>
           <span>{product.priceFormatted}</span>
           <button type="button" onClick={() => handleAddProduct(product.id)}>
             <div>
-              <MdAddShoppingCart size={16} color="#FFF"/> {' '}
+              <MdAddShoppingCart size={16} color="#FFF" />{' '}
               {amount[product.id] || 0}
             </div>
             <span>ADICIONAR AO CARRINHO</span>
